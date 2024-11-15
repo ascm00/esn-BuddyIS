56100dc7e84b755066f5483f1ce69cb2973c637e
ascm00 <martin.aschermann2@gmail.com>
2024-11-15 12:54:43 +0100

    Update registration-create-form.tsx

--- admin/app/components/forms/registration-create-form.tsx
+++ admin/app/components/forms/registration-create-form.tsx
@@ -52,9 +52,9 @@ export const RegistrationCreateForm = Component<RegistrationCreateFormProps>(
 
     // Check if something changed in the event capacity
     useEntityBeforePersist(() => {
-        if(isPaid && !isOnWaitingList){
-            // entity.updateValues({accepted: false})
-        }
+        // if(isPaid && !isOnWaitingList){
+        //      entity.updateValues({accepted: false})
+        // }
         
         const signedUpNumber = currentEvent.getField<number>('registeredCount.registered_count').value ?? 0
 
