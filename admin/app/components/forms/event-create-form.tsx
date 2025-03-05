ac89d0818353e6c221230218d61cc09d1193c6da
ascm00 <martin.aschermann2@gmail.com>
2025-03-05 21:36:19 +0100

    Update event-create-form.tsx

--- admin/app/components/forms/event-create-form.tsx
+++ admin/app/components/forms/event-create-form.tsx
@@ -33,7 +33,7 @@ export const EventCreateForm = Component(() => {
 		<InputField field="mapLink" label="Google maps link" description="Please add a valid link to the Google maps starting with 'https://' or 'http://'. Otherwise, it will not be displayed."/>
 		<InputField field="whatToBring" label="What to bring" />
 		<InputField field="whatsappLink" label="Whatsapp link" description="Please add a valid link to the WhatsApp group starting with 'https://' or 'http://'. Otherwise, it will not be displayed." />
-		<SelectField field={'contactPerson'} label="Contact person" options={'Person'}>
+		<SelectField field={'contactPerson'} label="Contact person" options={'Person[tenantPerson.roles = "admin" || tenantPerson.roles = "esnMember" || tenantPerson.roles = "coordinator"]'}>
 			<Field field={'firstName'} /> {' '} <Field field={'surname'} />
 		</SelectField>
 		{/* <ConnectUser field='contactPerson'>
