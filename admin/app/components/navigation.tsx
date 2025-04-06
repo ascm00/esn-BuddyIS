34ef822b62e44ab4a46802d2e7bf03baaabf8988
ascm00 <martin.aschermann2@gmail.com>
2025-04-06 19:58:00 +0200

    Update navigation.tsx

--- admin/app/components/navigation.tsx
+++ admin/app/components/navigation.tsx
@@ -26,9 +26,10 @@ export const Navigation = Component(
 		<MenuItem label="Home" icon={<Home className='text-blue-500' />} to="eventFeed" />
 		<MenuItem label="Events" icon={<CalendarPlus className='text-blue-500' />} to={'index'}>
 			<MenuItem label="All events" icon={<LayoutDashboard className='text-blue-500' />} to={'index'} />
+			<MenuItem label="Calendar" icon={<Calendar className='text-blue-500' />} to="calendar" />
+			<MenuItem label="My upcoming events" icon={<Clock className='text-blue-500' />} to="myEvents" />
 			<MenuItem label="Event list" icon={<List className='text-blue-500' />} to={'events'} />
 			<MenuItem label="N2N Party list" icon={<List className='text-blue-500' />} to="n2nParties" />
-			<MenuItem label="My upcoming events" icon={<Clock className='text-blue-500' />} to="myEvents" />
 		</MenuItem>
 	</HasRole>
 	<HasRole role={roles => roles.has('admin') || roles.has('esnMember') || roles.has('czechBuddy') || roles.has('coordinator')}>
