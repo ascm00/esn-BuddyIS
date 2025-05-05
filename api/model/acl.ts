8ed22618c43307cd2ce02034c40aee72da81e2e8
ascm00 <martin.aschermann2@gmail.com>
2025-05-05 13:17:23 +0200

    acl edits

--- api/model/acl.ts
+++ api/model/acl.ts
@@ -1,10 +1,10 @@
 import { c } from '@contember/schema-definition'
 
-export const internationalStudentRole = c.createRole('internationalStudent', { stages: '*', s3: { '**': { upload: true, read: true } }, tenant: { invite: { internationalStudentRole: true }}})
+export const internationalStudentRole = c.createRole('internationalStudent', { stages: '*', s3: { '**': { upload: true, read: true } }, tenant: { invite: { czechBuddyRole: true, internationalStudentRole: true }}})
 export const esnMemberRole = c.createRole('esnMember', { stages: '*', s3: { '**': { upload: true, read: true } } })
 export const paymentGateRole = c.createRole('paymentGate', { stages: '*', s3: { '**': { upload: true, read: true } } })
 export const ozsRole = c.createRole('ozsRole', { stages: '*', s3: { '**': { upload: true, read: true } } })
-export const czechBuddyRole = c.createRole('czechBuddy', { stages: '*', s3: { '**': { upload: true, read: true } }, tenant: { invite: { czechBuddyRole: true },} })
+export const czechBuddyRole = c.createRole('czechBuddy', { stages: '*', s3: { '**': { upload: true, read: true } }, tenant: { invite: { czechBuddyRole: true, internationalStudentRole: true },} })
 export const coordinatorRole = c.createRole('coordinator', { stages: '*', s3: { '**': { upload: true, read: true } } })
 export const emailCheckRole = c.createRole('emailCheck', { stages: '*', s3: { '**': { upload: true, read: true } } })
 
