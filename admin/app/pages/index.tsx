f42effccba876efd52ab7b95336daaa28f5a2ca9
ascm00 <martin.aschermann2@gmail.com>
2025-09-11 16:53:59 +0200

    Update index.tsx

--- admin/app/pages/index.tsx
+++ admin/app/pages/index.tsx
@@ -1,15 +1,20 @@
 import { useEffect } from 'react'
-import { HasRole, useProjectUserRoles, useRedirect } from '@contember/interface'
+import { HasRole, Link, useProjectUserRoles, useRedirect } from '@contember/interface'
 import { Binding } from '@app/lib/binding'
 import { InitialUserForm } from '@app/components/initialUserForm'
 import InitialUserPage from './initialUserPage'
 import EventFeed from './eventFeed'
 import BuddyPairs from './buddyPairs'
 import Dashboard from './dashboard'
+import { Slots } from '@app/lib/layout'
+import { Button } from '@app/lib/ui/button'
+import { PlusCircle } from 'lucide-react'
+import { useIsMobile } from '@app/lib/utils/use-mobile'
 
 export default () => {
 	const redirect = useRedirect()
 	const roles = useProjectUserRoles()
+	const isMobile = useIsMobile()
 
 	useEffect(() => {
 		}, [redirect])
@@ -20,6 +25,16 @@ export default () => {
 					<BuddyPairs />
 				</HasRole>
 				<HasRole role={roles => roles.has('admin') || roles.has('coordinator') || roles.has('internationalStudent') || roles.has('czechBuddy') || roles.has('esnMember')}>
+					<HasRole role={roles => roles.has('admin') || roles.has('esnMember') || roles.has('coordinator')}>
+              			<Slots.Actions>
+							<Link to="eventCreate">
+								<Button>
+									{!isMobile && 'Create event'}
+									{isMobile && <PlusCircle />}
+								</Button>
+							</Link>
+						</Slots.Actions>
+          			</HasRole>
 					<Dashboard />
 				</HasRole>
 			</>
